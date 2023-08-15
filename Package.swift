862db24fa2b35c01a38d7fd0d93440392e8227ef
genexusbot <builder@genexus.com>
2023-08-15 10:25:10 -0300

    Package version: 1.1.0

---
+++ Package.swift
@@ -0,0 +1,32 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+	name: "GXCertificatePinning",
+	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	products: [
+		.library(
+			name: "GXCertificatePinning",
+			targets: ["GXCertificatePinningWrapper"])
+	],
+	dependencies: [
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0")
+	],
+	targets: [
+		.target(name: "GXCertificatePinningWrapper",
+				dependencies: [
+					"GXCertificatePinning",
+					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+				],
+				path: "Sources"),
+		.binaryTarget(
+			name: "GXCertificatePinning",
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCertificatePinning-1.1.0.xcframework.zip",
+			checksum: "598ad812c2c50e7d35e647f65983865ae8a26b6f40953c37e6b663a7674b26a8"
+		)
+	]
+)
