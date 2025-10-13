a3c52c3a541cdb737d93cd1c1b3cf340c8e56e74
The iOS Team @ GeneXus <builder@genexus.com>
2025-10-13 04:49:46 -0400

    Package version: 4.0.0

--- Package.swift
+++ Package.swift
@@ -1,18 +1,19 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
 	name: "GXCertificatePinning",
-	platforms: [.iOS("12.0"), .watchOS("5.0"), .tvOS("12.0")],
+	platforms: [.iOS("15.0"), .watchOS("10.0"), .tvOS("18.0")],
 	products: [
 		.library(
 			name: "GXCertificatePinning",
 			targets: ["GXCertificatePinningWrapper"])
 	],
 	dependencies: [
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "1.1.0"),
-		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "1.1.0")
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreBL.git", exact: "4.0.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXCoreUI.git", exact: "4.0.0"),
+		.package(url: "https://github.com/GeneXus-SwiftPackages/GXFoundation.git", exact: "4.0.0"),
+		.package(url: "https://github.com/datatheorem/TrustKit", .upToNextMinor(from: "3.0.4"))
 	],
 	targets: [
 		.target(name: "GXCertificatePinningWrapper",
@@ -20,13 +21,14 @@ let package = Package(
 					"GXCertificatePinning",
 					.product(name: "GXCoreBL", package: "GXCoreBL", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
 					.product(name: "GXCoreUI", package: "GXCoreUI", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
-					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
+					.product(name: "GXFoundation", package: "GXFoundation", condition: .when(platforms: [.iOS, .watchOS, .tvOS])),
+					.product(name: "TrustKitDynamic", package: "TrustKit", condition: .when(platforms: [.iOS, .watchOS, .tvOS]))
 				],
 				path: "Sources"),
 		.binaryTarget(
 			name: "GXCertificatePinning",
-			url: "https://pkgs.genexus.dev/iOS/releases/GXCertificatePinning-1.1.0.xcframework.zip",
-			checksum: "598ad812c2c50e7d35e647f65983865ae8a26b6f40953c37e6b663a7674b26a8"
+			url: "https://pkgs.genexus.dev/iOS/releases/GXCertificatePinning-4.0.0.xcframework.zip",
+			checksum: "54941d4665d225a32020926d53ff7aad2fe1a84689aa4a0816779e0a91eaf2c9"
 		)
 	]
 )
